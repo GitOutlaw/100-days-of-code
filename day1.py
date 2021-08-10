835809cfc24dba4e370a6b2b2d3ba349575923fe
GitOutlaw <lrt29net@gmail.com>
2021-08-10 11:19:03 -0400

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,7 @@
+print('Welcome to the Band Name Generator')
+
+city_name = input('What is the name of the city you grew up in?: ')
+pet_name = input('What is your pets name?: ')
+
+
+print(f'Your band name could be {city_name} {pet_name}')
