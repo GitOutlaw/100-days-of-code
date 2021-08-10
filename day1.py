47982bba6255c98856124f575f39cffe0e2db6b0
GitOutlaw <lrt29net@gmail.com>
2021-08-10 13:39:45 -0400

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,7 +1,7 @@
 print('Welcome to the Band Name Generator')
 
-city_name = input('What\'s the name of the city you grew up in?: ')
-pet_name = input('What\'s your pets name?: ')
+city_name = input('What\'s the name of the city you grew up in?: \n')
+pet_name = input('What\'s your pets name?: \n')
 
 
 print(f'Your band name could be {city_name} {pet_name}')
