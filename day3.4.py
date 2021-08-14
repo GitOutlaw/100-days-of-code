266a7ebaeeb60b50085ce4b7c49684b88fb1dbaa
GitOutlaw <lrt29net@gmail.com>
2021-08-14 14:43:09 -0400

    Update day3.4.py

--- day3.4.py
+++ day3.4.py
@@ -1,5 +1,7 @@
 print("Welcome to Python Pizza Deliveries")
 size = input("What size pizza do you want? S, M, or L: ")
+add_pepperoni = input("Do you want pepperoni? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
 
 price = 0
 
@@ -12,21 +14,15 @@ elif size == "M":
 elif size == "L":
     price = 25.00
 
-add_pepperoni = input("Do you want pepperoni? Y or N: ")
-
 if add_pepperoni == "Y" and size == "S":
     price += 2.00
-    print(f"Your final bill is: ${price:.2f}")
+
 elif add_pepperoni == "Y" and size == "L" or "M":
     price += 3.00
-    print(f"Your final bill is: ${price:.2f}")
 
-add_extra_cheese = input("Do you want extra cheese/ Y or N: ")
-
-if add_extra_cheese == "Y":
+if extra_cheese == "Y":
     price += 1.00
-    print(f"Your final bill is: ${price:.2f}")
+    print(f"Your final bill is: ${price:.2f}.")
 
-elif add_extra_cheese == 'Y' and  :
-    price += 
-
+else:
+    print(f"Your final bill is: ${price:.2f}.")
