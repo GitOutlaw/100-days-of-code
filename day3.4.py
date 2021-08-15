d8b2fe766ad87bc4c0c1aa1076747f9ca7d09cce
GitOutlaw <lrt29net@gmail.com>
2021-08-15 13:52:18 -0400

    Update day3.4.py

--- day3.4.py
+++ day3.4.py
@@ -3,26 +3,26 @@ size = input("What size pizza do you want? S, M, or L: ")
 add_pepperoni = input("Do you want pepperoni? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
-price = 0
+bill = 0
 
 if size == "S":
-    price = 15.00
+    bill = 15.00
 
 elif size == "M":
-    price = 20.00
+    bill = 20.00
 
 elif size == "L":
-    price = 25.00
+    bill = 25.00
 
 if add_pepperoni == "Y" and size == "S":
-    price += 2.00
+    bill += 2.00
 
 elif add_pepperoni == "Y" and size == "L" or "M":
-    price += 3.00
+    bill += 3.00
 
 if extra_cheese == "Y":
-    price += 1.00
-    print(f"Your final bill is: ${price:.2f}.")
+    bill += 1.00
+    print(f"Your final bill is: ${bill:.2f}.")
 
 else:
-    print(f"Your final bill is: ${price:.2f}.")
+    print(f"Your final bill is: ${bill:.2f}.")
