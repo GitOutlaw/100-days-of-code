a4f93655e5246ea7c3019357c17c29d0f16d64e6
GitOutlaw <lrt29net@gmail.com>
2021-08-15 15:01:18 -0400

    Update day3.a.py

--- day3.a.py
+++ day3.a.py
@@ -11,14 +11,16 @@ if height >= 47:
     elif age <= 18:
         bill = 7.00
         print("Youth tickets are $7.00.")
+    elif age >= 45 and age <= 55:
+        bill = 0.00
+        print("Everything is going to be ok. Have a free ride on us!")
     else:
         bill = 12.00
         print("Adult tickets are $12.00.")
 
-    wants_photo = input('Do you want a photo? Y or N: ')
-    if wants_photo == 'Y':
+    wants_photo = input("Do you want a photo? Y or N: ")
+    if wants_photo == "Y":
         bill += 3.00
-
     print(f"Your final bill is ${bill:.2f}")
 
 else:
