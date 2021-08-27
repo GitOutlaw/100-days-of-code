150172254c86f2bc97b8ebc650429efa21d02fe5
GitOutlaw <lrt29net@gmail.com>
2021-08-27 15:46:24 -0400

    Update day7.5.py
    cleaned code and added clear function from replit

--- day7/day7.5.py
+++ day7/day7.5.py
@@ -1,19 +1,15 @@
 # Step 5
 
+from replit import clear
 import random
 from hangman_words import word_list
-from hangman_art import stages
-from hangman_art import logo
-
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+from hangman_art import logo, stages
 
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
-
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 
 print(logo)
 
@@ -28,13 +24,11 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    clear()
 
     # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(
-            f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
         elif guess in guess:
@@ -42,11 +36,10 @@ while not end_of_game:
 
     # Check if user is wrong.
     if guess not in chosen_word:
-        # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+
+        print(
+            f"You guessed {guess}, that's not in the word. You lose a life")
         lives -= 1
-        if guess not in chosen_word:
-            print(
-                f"You guessed {guess}, that's not in the word. You lose a life")
 
         if lives == 0:
             end_of_game = True
@@ -60,5 +53,4 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    # TODO-2: - Import the stages from hangman_art.py and make this error go away.
     print(stages[lives])
