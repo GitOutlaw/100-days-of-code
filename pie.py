607eb70b3b8d0a93e81e90f72afcb29e33c9220d
GitOutlaw <lrt29net@gmail.com>
2021-08-30 11:49:17 -0400

    Updates

---
+++ pie.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+cipher_text = ""
+
+for letter in text:
+
+    position = alphabet.index(letter)
+
+    shifting_number = (position + shift) % 26
+
+    cipher_text += alphabet[shifting_number]
+
+print(f"The encoded text is: {cipher_text}")
