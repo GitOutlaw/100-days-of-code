18f3c1fc06a9675d1d688c110d35e6d5d088b535
GitOutlaw <lrt29net@gmail.com>
2021-09-15 17:12:59 -0400

    Update higher_lower_game.py

--- day14/higher_lower_game.py
+++ day14/higher_lower_game.py
@@ -1,33 +1,25 @@
-from higher_lower_art import logo, vs
 from higher_lower_game_data import data
-from replit import clear
+from higher_lower_art import logo, vs
 import random
-
-
-def get_random_account():
-    """Gets data from random account."""
-
-    return random.choice(data)
+from replit import clear
 
 
 def format_data(account):
-    """Formats data and returns fstring with variables"""
-
-    name = account["name"]
-    description = account["description"]
-    country = account["country"]
-    answer = account["follower_count"]
-
-    # Used for testing. Shows how "follower_count" from data.
-    return f"{name}, a {description}, from {country}. Pssst the answer: {answer}"
-
-    # return f"{name}, a {description}, from {country}."
+    """Takes the account data and returns the printable format."""
+    account_name = account["name"]
+    account_description = account["description"]
+    account_country = account["country"]
+    
+    # Used to debug the game.
+    account_answer = account["follower_count"]
+    print(f"Pssst the answer is: {account_answer}")
+    
+    # Used to return fstring with formatted data.
+    return f"{account_name}, a {account_description}, from {account_country}."
 
 
 def check_answer(guess, a_followers, b_followers):
-    """Checks follower_count against user's guess 
-    and returns True if it's right.
-    Or False if it's wrong."""
+    """Take the users guess and follower counts and return if they got it right"""
     if a_followers > b_followers:
         return guess == "a"
     else:
@@ -35,59 +27,51 @@ def check_answer(guess, a_followers, b_followers):
 
 
 def game():
-    """Executes higher lower game. """
-    # Prints logo from higher_lower_art.py
-    print(logo)
 
-    # Sets intial score to 0
+    # Set intial score to 0
     score = 0
-
-    # Sets game_should_continue intial value
+    # Used for while loop
     game_should_continue = True
+    account_b = random.choice(data)
 
-    # Outputs
-    account_a = get_random_account()
-    account_b = get_random_account()
-
-    # First while statement - game_should_ continue
+    # Makes the game repeatable.
     while game_should_continue:
-        #
+        # Generate a random account from the game data.
         account_a = account_b
-        account_b = get_random_account()
+        account_b = random.choice(data)
 
-        # Nested while loop
+        # Makes account at postition B become the next account at position A.
         while account_a == account_b:
-            account_b = get_random_account()
+            account_b = random.choice(data)
 
-        # Prints formated_data in fstring
+        # Print formatted accounts from data.
         print(f"Compare A: {format_data(account_a)}.")
         print(vs)
-        print(f"Against: B: {format_data(account_b)}")
+        print(f"Against B: {format_data(account_b)}.")
 
-        # User input requesting guess, then format using .lower()
-        guess = input("Who has more followers? Type 'A' or 'B':").lower()
+        # Ask user for a guess.
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-        # Gets follower count from higher_lower_data
+        # Gets follower count of each account. Checks if user is correct. 
         a_follower_count = account_a["follower_count"]
         b_follower_count = account_b["follower_count"]
-
-        # Checks the answer from check_answer()
         is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-        # Executes the clear() function from replit
+        # Clear the screen between rounds
         clear()
-
-        # Prints logo again from higher_lower_art.py
+        # Display art.
         print(logo)
 
-        # Scores the game
+        # Give user feedback on their guess, and keep score.
         if is_correct:
             score += 1
-            print(f"You're right! Current score: {score}.")
+            print(f"You're right! Current score: {score}")
+            
 
         else:
             game_should_continue = False
-            print(f"Sorry, that's wrong. Final score {score}")
-
+            print(f"Sorry that's wrong. Final score: {score}")
 
 game()
+
+
