d2df3b2b93b80bd5cbbf9b3a6e5918ac8d051904
GitOutlaw <lrt29net@gmail.com>
2021-09-18 10:35:30 -0400

    Updates

---
+++ day15/coffee-machine-start.py
@@ -0,0 +1,39 @@
+from coffee_machine_data import MENU, resources, coins
+
+# Print report.
+
+
+def resource_report():
+
+    water = resources["water"]
+    milk = resources["milk"]
+    coffee = resources["coffee"]
+
+    print(f"Water: {water}ml")
+    print(f"Milk: {milk}ml")
+    print(f"Coffee: {coffee}g")
+
+
+# report()
+
+# Check resources sufficient?
+
+# Process coins.
+
+# Check transaction successful.
+
+
+
+user_prompt = input("What would you like? (espresso/latte/cappuccino): ")
+print("Please insert coins.")
+money_quarters = int(input("How many quarters?: "))
+money_dimes = int(input(f"How many dimes?: "))
+money_nickels = int(input(f"How many pennies?: "))
+
+#
+if user_prompt == "espresso":
+
+
+#  
+
+
