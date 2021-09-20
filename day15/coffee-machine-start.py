ea7e5ad6a452b9c738c892676ef32468be36c874
GitOutlaw <lrt29net@gmail.com>
2021-09-20 16:11:20 -0400

    Update
    Created coffee-machine.py version 1

--- day15/coffee-machine-start.py
+++ day15/coffee-machine-start.py
@@ -1,39 +1,124 @@
-from coffee_machine_data import MENU, resources, coins
+# from coffee_machine_data import MENU, resources, coins
 
-# Print report.
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO: 1. User input asking the menu
+# TODO: 2. Turning off for maintenance
+# TODO: 3. Check resources using report
+# TODO: 4. Check enough resources to make coffee
 
 
-def resource_report():
+def resource_check(drink, ingredient_type):
+    ingredients_check = MENU[drink]['ingredients'][ingredient_type]
+    resources_check = resources[ingredient_type]
 
-    water = resources["water"]
-    milk = resources["milk"]
-    coffee = resources["coffee"]
+    if ingredients_check <= resources_check:
+        return True
+    elif ingredients_check > resources_check:
+        return False
 
-    print(f"Water: {water}ml")
-    print(f"Milk: {milk}ml")
-    print(f"Coffee: {coffee}g")
+def check(water_check, milk_check, coffee_check):
 
-
-# report()
-
-# Check resources sufficient?
-
-# Process coins.
-
-# Check transaction successful.
-
-
-
-user_prompt = input("What would you like? (espresso/latte/cappuccino): ")
-print("Please insert coins.")
-money_quarters = int(input("How many quarters?: "))
-money_dimes = int(input(f"How many dimes?: "))
-money_nickels = int(input(f"How many pennies?: "))
-
-#
-if user_prompt == "espresso":
+    if water_check:
+        if milk_check:
+            if coffee_check:
+                print('Please insert coins.')
+                return True
+            else:
+                print(f"Sorry there is not enough coffee.")
+        else:
+            print(f"Sorry there is not enough milk.")
+    else:
+        print(f"Sorry there is not enough water.")
 
 
-#  
+# TODO: 5. Input coins by user
+
+def money(name_of_coins):
+    ask_money = int(input(f"how many {name_of_coins}?: "))
+    return ask_money
+
+
+# TODO: 6. Calculate transaction
+
+def calculate_transaction(money_insufficient, give_change, same, change):
+    if money_insufficient:
+        print("Sorry that's not enough money. Money refunded.")
+        continue_serving = False
+    elif give_change:
+        print(f"Here is ${change} dollars in change.")
+        return True
+    elif same:
+        return True
+
+# TODO: 7. Add profit before making coffee
+# TODO: 8-1. Make coffee deduct ingredients
+
+def ingredients_deduction(drink, ingredient_type):
+    resources[ingredient_type] -= MENU[drink]['ingredients'][ingredient_type]
+
+#TODO: 8-2.  Print serving
+#TODO: 10. serve the next customer by repeating from todo1
+
+def menu():
+    user_choice = input(" What would you like? (espresso/latte/cappuccino): ")
+
+    if user_choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        
+        
+
+    elif user_choice == "espresso":
+        resources["water"] -= 50
+        resources["coffee"] -= 18
+        
+
+    elif user_choice == "latte":
+        resources["water"] -= 200
+        resources["milk"] -= 150
+        resources["coffee"] -= 24
+        
+
+    elif user_choice == "cappuccino":
+        resources["water"] -= 250
+        resources["milk"] -= 100
+        resources["coffee"] -= 24
+    
+# elif user_choice == "off":
+    
+
+
 
 
---
+++ day15/snips.py
@@ -0,0 +1,42 @@
+    # def insert_coins():
+#     print("Please insert coins.")
+#     quarters = int(input("How many quarters?: "))
+#     dimes = int(input("How many dimes?: "))
+#     nickels = int(input("How many nickels?: "))
+#     pennies = int(input("How many pennies?: "))   
+
+#     return quarters, dimes, nickels, pennies 
+
+
+# def calculate_coins():
+#     quarters, dimes, nickels, pennies = insert_coins()    
+#     total_quarters = quarters * 0.25
+#     total_dimes = dimes * 0.10
+#     total_nickels = nickels * 0.05
+#     total_pennies = pennies * 0.01
+        
+#     total_coins = total_quarters + total_dimes + total_nickels + total_pennies    
+
+#     print(f"${total_coins}0")
+
+#     return total_coins
+
+
+    
+    
+    
+    # elif menu == "latte":
+    #     if resources["water"] > 200 and resources["milk"] > 150 and resources["coffee"] >= 24:
+    #         resources["water"] -= 200
+    #         resources["milk"] -= 150
+    #         resources["coffee"] -= 24
+    #         print("Here is your latte")
+    #     else:
+    #         print("Sorry not enough resources")
+    # elif menu == "cappuccino":
+    #     if resources["water"] > 250 and resources["milk"] > 100 and resources["coffee"] > 24:
+    #         resources["water"] -= 250
+    #         resources["milk"] -= 100
+    #         resources["coffee"] -= 24        
+    #         print("Here is your cappuccino")
+    #     else:
