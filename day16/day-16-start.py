bca1d11923f80b248aadc4c7e4a18f5825ade321
GitOutlaw <lrt29net@gmail.com>
2021-09-22 11:11:45 -0400

    Updates

---
+++ day16/day-16-start.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+john = Turtle()
+print(john)
+john.shape("turtle")
+john.color("red", "green")
+john.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
