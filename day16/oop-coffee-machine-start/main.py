1a5bbbb882f7f8bb91434486d83c41f1caf08a99
GitOutlaw <lrt29net@gmail.com>
2021-09-23 15:20:10 -0400

    Update main.py

--- day16/oop-coffee-machine-start/main.py
+++ day16/oop-coffee-machine-start/main.py
@@ -11,17 +11,21 @@ is_on = True
 while is_on:
 
     option = menu.get_items()
-    choice = input(f"What would you like? {option} ")
+    choice = input(f"What would you like? {option}")
 
     if choice == "off":
         is_on = False
-    elif choice == "report":
+    elif choice ==  "report":
         coffee_maker.report()
         money_machine.report()
     else:
         drink = menu.find_drink(choice)
         is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
-        is_payment_successful = money_machine.make_payment(drink.cost)
+        is_payment_successfull = money_machine.make_payment(drink.cost)
 
-        if is_enough_ingredients and is_payment_successful:
+        if is_enough_ingredients and is_payment_successfull:
             coffee_maker.make_coffee(drink)
+
+
+
+
