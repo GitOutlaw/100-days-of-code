e4997711c678df890425e6c0fbb07376b1344ee9
GitOutlaw <lrt29net@gmail.com>
2021-09-23 14:22:16 -0400

    Update main.py

--- day16/oop-coffee-machine-start/main.py
+++ day16/oop-coffee-machine-start/main.py
@@ -2,13 +2,12 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
 
 is_on = True
- 
+
 while is_on:
 
     option = menu.get_items()
@@ -23,6 +22,6 @@ while is_on:
         drink = menu.find_drink(choice)
         is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
         is_payment_successful = money_machine.make_payment(drink.cost)
-        
+
         if is_enough_ingredients and is_payment_successful:
             coffee_maker.make_coffee(drink)
