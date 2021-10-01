4168feaa948480b98f5b0168699a8ecc277b1482
GitOutlaw <lrt29net@gmail.com>
2021-10-01 12:42:39 -0400

    Update day18b.py

--- day18/day18b.py
+++ day18/day18b.py
@@ -8,16 +8,16 @@ john.pensize(6)
 john.hideturtle()
 john.speed('fastest')
 
+# Tuple with directions east, north, west & south represented numerically.
+DIRECTIONS = (0, 90, 180, 270)
 # List used to select random color.
 colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
           "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-# Sets direction east, north, west & south.
-DIRECTIONS = [0, 90, 180, 270]
 
 for _ in range(200):
-    john.color(random.choice(colors)) # Chooses random color.
-    john.setheading(random.choice(DIRECTIONS)) # Chooses random direction.
-    john.forward(10) # Moves the turtle forward.
+    john.color(random.choice(colors))  # Chooses random color.
+    john.setheading(random.choice(DIRECTIONS))  # Chooses random direction.
+    john.forward(10)  # Moves the turtle forward.
 
-screen = Screen() # Instantiates the Screen() function.
-screen.exitonclick() # Sets the window to be closed by user pressing close.
+screen = Screen()  # Instantiates the Screen() function.
+screen.exitonclick()  # Sets the window to be closed by user pressing close.
