cc3792db67b8e198190ae891310c025babc3d075
John Crockett <58158920+GitOutlaw@users.noreply.github.com>
2021-10-25 16:38:32 -0400

    Update
    Created US States game with Pandas and Turtle modules.

--- day25/squirrel_count/main.py
+++ day25/squirrel_count/main.py
@@ -2,7 +2,6 @@ import pandas
 
 data = pandas.read_csv("day25/squirrel_count/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
-
 gray_squirels_count = len(data[data["Primary Fur Color"] == "Gray"])
 red_squirels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
 black_squirels_count = len(data[data["Primary Fur Color"] == "Black"])
